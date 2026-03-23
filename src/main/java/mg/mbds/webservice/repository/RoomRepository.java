1e8caf19388e441a72f09c68e381f01f12478c12
Nancia Rajerison <rajerisonnancia@gmail.com>
2026-03-23 22:32:35 +0300

    Add filters to the avalaible room list

--- src/main/java/mg/mbds/webservice/repository/RoomRepository.java
+++ src/main/java/mg/mbds/webservice/repository/RoomRepository.java
@@ -2,18 +2,9 @@ package mg.mbds.webservice.repository;
 
 import mg.mbds.webservice.model.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
-public interface RoomRepository extends JpaRepository<Room, Long> {
-
-    @Query("""
-            SELECT r FROM Room r
-            WHERE r.underMaintenance = false
-            AND (SELECT COUNT(s) FROM Stay s WHERE s.room = r AND s.endDate IS NULL) < r.capacity
-            """)
-    List<Room> findAvailableRooms();
+public interface RoomRepository extends JpaRepository<Room, Long>, JpaSpecificationExecutor<Room> {
 }
--- src/main/java/mg/mbds/webservice/service/RoomService.java
+++ src/main/java/mg/mbds/webservice/service/RoomService.java
@@ -1,9 +1,13 @@
 package mg.mbds.webservice.service;
 
 import mg.mbds.webservice.model.Room;
+import mg.mbds.webservice.model.RoomType;
 import mg.mbds.webservice.repository.RoomRepository;
+import mg.mbds.webservice.repository.RoomSpecification;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
@@ -15,7 +19,18 @@ public class RoomService {
         this.roomRepository = roomRepository;
     }
 
-    public List<Room> getAvailableRooms() {
-        return roomRepository.findAvailableRooms();
+    public List<Room> getAvailableRooms(RoomType type, Integer minCapacity,
+                                        Double minPrice, Double maxPrice,
+                                        LocalDate date) {
+        LocalDate effectiveDate = date != null ? date : LocalDate.now();
+
+        Specification<Room> spec = Specification
+                .where(RoomSpecification.isAvailableOn(effectiveDate))
+                .and(RoomSpecification.hasType(type))
+                .and(RoomSpecification.hasMinCapacity(minCapacity))
+                .and(RoomSpecification.hasPriceGreaterThanOrEqual(minPrice))
+                .and(RoomSpecification.hasPriceLessThanOrEqual(maxPrice));
+
+        return roomRepository.findAll(spec);
     }
 }
